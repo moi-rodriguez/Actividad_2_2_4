7a467c4548295f9e2d9ea43a8b0af7662898dd94
moi.rodriguez <moi.rodriguez@duocuc.cl>
2025-10-26 12:44:53 -0300

    Modificación de dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("andoridx.lyfecicle:lifecycle-viewmodel-compose:2.8.0-rc02")
+    implementation("androidx.lifecyle:lifecycle-runtime-compose:2.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
